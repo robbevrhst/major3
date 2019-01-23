33c455993c4e284ebfd63ea448c2502fdc54623f
Robbe Verhoest <robbe.verhoest@student.howest.be>
2019-01-24 00:43:05 +0100

    mobile header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+{
+  const mainNav = document.getElementById('js-menu');
+  const navBarToggle = document.getElementById('js-navbar-toggle');
+
+  navBarToggle.addEventListener('click', function() {
+    mainNav.classList.toggle('active');
+  });
+}
